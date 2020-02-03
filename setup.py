b2742969034d043d363caa328a8669658f88085d
Christian Versloot <mail@christianversloot.nl>
2020-02-03 09:51:56 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name = 'extra_keras_datasets',
   packages = ['extra_keras_datasets'],
-  version = '0.1.5',
+  version = '0.1.6',
   license='MIT',
   description = 'Extending the Keras Datasets module with extra ones.',
   long_description = 'Extending the Keras Datasets module with extra ones.',
   author = 'Christian Versloot',
   author_email = 'chris@machinecurve.com',
   url = 'https://github.com/christianversloot/extra_keras_datasets',
-  download_url = 'https://github.com/christianversloot/extra_keras_datasets/archive/0.1.5.tar.gz',
+  download_url = 'https://github.com/christianversloot/extra_keras_datasets/archive/0.1.6.tar.gz',
   keywords = ['keras', 'datasets', 'machine learning'],
   install_requires=[
     'tensorflow-auto-detect',
