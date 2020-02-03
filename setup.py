db9893299c23ea044c016f27cb3fc6c2e1a6a9cf
Christian Versloot <mail@christianversloot.nl>
2020-02-03 08:47:49 +0100

    Use Tensorflow 2.0

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from distutils.core import setup
 setup(
   name = 'extra_keras_datasets',
   packages = ['extra_keras_datasets'],
-  version = '0.1.4',
+  version = '0.1.5',
   license='MIT',
   description = 'Extending the Keras Datasets module with extra ones.',
   long_description = 'Extending the Keras Datasets module with extra ones.',
   author = 'Christian Versloot',
   author_email = 'chris@machinecurve.com',
   url = 'https://github.com/christianversloot/extra_keras_datasets',
-  download_url = 'https://github.com/christianversloot/extra_keras_datasets/archive/0.1.4.tar.gz',
+  download_url = 'https://github.com/christianversloot/extra_keras_datasets/archive/0.1.5.tar.gz',
   keywords = ['keras', 'datasets', 'machine learning'],
   install_requires=[
-    'keras',
+    'tensorflow>=2.0.0',
     'numpy',
     'scipy'
   ],
